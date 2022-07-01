cd9879a6d436cf78f5c587f6ad7d336b079ebc1e
Vlad Shchelkunov <specter.ode@gmail.com>
2022-07-01 12:48:18 +0300

    start!

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,64 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// npm install
+// https://github.com/Specter-ode/goit-js-hw-08
+
+// npm install simplelightbox
+// npm install @vimeo/player
+// npm i lodash.throttle
+
+
+
+
+
+
+
+
+const galleryContainer = document.querySelector('.gallery');
+const blockMarkUp = createImagesGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("afterbegin", blockMarkUp);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createImagesGallery (items) {
+    return items
+    .map ( ({ preview, original, description }) =>
+      `<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+              <img class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+              />
+          </a>
+      </div>`)
+      .join('');
+};
+  
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}">`,
+        {
+            onShow: function () { 
+                window.addEventListener("keydown", checkKeyEvent)
+            },
+            onClose: function () {
+                window.removeEventListener("keydown", checkKeyEvent)
+            }
+        }
+    )
+    instance.show()
+
+    function checkKeyEvent (event) {
+        console.log(event.code)
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    }
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
 
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const options = {
+  id: 59777392,
+  width: 640,
+  loop: true,
+};
